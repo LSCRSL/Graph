44e76417706d17bb2e31db597038dc5c6aae2abe
Elisa Ciocarlan <eciocar@a-999903.ups.u-psud.fr>
2023-01-17 16:23:21 +0100

    création

---
+++ tests/open_digraph_test.py
@@ -0,0 +1,26 @@
+import sys
+import os
+root = os.path.normpath(os.path.join(__file__, './../..'))
+sys.path.append(root)# allows us to fetch files from the project root
+import unittest
+from modules.open_digraph import *
+
+class InitTest(unittest.TestCase):
+    def test_init_node(self):
+        n0 = node(0, 'i', {}, {1:1})
+        self.assertEqual(n0.id, 0)
+        self.assertEqual(n0.label, 'i')
+        self.assertEqual(n0.parents, {})
+        self.assertEqual(n0.children, {1:1})
+        self.assertIsInstance(n0, node)
+        
+class NodeTest(unittest.testcase):
+    def setUp(self):
+        self.n0 = node(0, 'a', [], [1])
+    def test_get_id(self):
+        self.assertEqual(self.n0.get_id(), 0)
+    def test_get_label(self):
+        self.assertEqual(self.n0.get_label(), 'a')
+        
+if __name__ == '__main__': # the following code is called only when
+    unittest.main() # precisely this file is run
